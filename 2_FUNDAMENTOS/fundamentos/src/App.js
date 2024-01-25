5326ec6c501a9ad235a668646b5e5acbb9cc5739
Joilson Jesus <joilsondev2023@gmail.com>
2024-01-25 11:16:15 -0300

    first

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,18 @@
+//COMPONENTS
+  import FirstComponent from "./components/FirstComponent"
+
+
+//Styles / CSS
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+          </div>
+        
+      
+  );
+}
+
+export default App
